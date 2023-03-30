01868f9c9af98051049c650ef093105479b2f3ec
Hammyboi <honglinjamesmou@gmail.com>
2023-03-29 21:02:24 -0400

    Not fully done adding website for QR program

---
+++ QR Scouting/Website Files/_next/static/Agt1pCHjBIACAPmJNZkZ8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/308-da86da77d1136b18.js","static/chunks/pages/index-17c4077026a3c3e1.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
