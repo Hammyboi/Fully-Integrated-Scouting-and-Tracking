3a4f9797acec18cae12ccec4fd2c821aa1f84377
Hammyboi <honglinjamesmou@gmail.com>
2023-05-20 23:02:42 -0400

    backend updated to be more flexible

--- Integrated Scouting System/backend.php
+++ Integrated Scouting System/backend.php
@@ -5,6 +5,9 @@
     $robot = $_GET['robot'];
     $teamnumber = $_GET['teamnumber'];
     $noshow = $_GET['noshow'];
+if(empty($noshow)){
+    $noshow = 0;
+}
     $location = $_GET['location'];
     $auto_upper_cone = $_GET['auto_upper_cone'];
     $auto_middle_cone = $_GET['auto_middle_cone'];
@@ -13,7 +16,13 @@
     $auto_middle_cube = $_GET['auto_middle_cube'];
     $auto_lower_cube = $_GET['auto_lower_cube'];
     $mobility = $_GET['mobility'];
+if(empty($mobility)){
+    $mobility = 0;
+}
     $auto_pickup = $_GET['auto_pickup'];
+if(empty($auto_pickup)){
+    $auto_pickup = 0;
+}
     $auto_dock_engage = $_GET['auto_dock_engage'];
     $teleop_upper_cone = $_GET['teleop_upper_cone'];
     $teleop_middle_cone = $_GET['teleop_middle_cone'];
@@ -37,14 +46,29 @@
     }
     $links = $_GET['links'];
     $early_endgame = $_GET['early_endgame'];
+if(empty($early_endgame)){
+    $early_endgame = 0;
+}
     $endgame = $_GET['endgame'];
     $num_docked = $_GET['num_docked'];
     $skill = $_GET['skill'];
     $defense = $_GET['defense'];
     $died = $_GET['died'];
+if(empty($died)){
+    $died = 0;
+}
     $tipped = $_GET['tipped'];
+if(empty($tipped)){
+    $tipped = 0;
+}
     $card = $_GET['card'];
+if(empty($card)){
+    $card = 0;
+}
     $damage = $_GET['damage'];
+if(empty($damage)){
+    $damage = 0;
+}
     $comments = $_GET['comments'];
     $combined = $names . $match . $robot . $teamnumber;
     $md5 = md5($combined);
@@ -73,13 +97,13 @@
     $insert = "INSERT INTO input_from_site VALUES ('$names', '$match', '$robot', '$teamnumber', '$noshow', '$location', '$mobility', '$auto_upper_cone', '$auto_middle_cone', '$auto_lower_cone', '$auto_upper_cube', '$auto_middle_cube', '$auto_lower_cube', '$auto_pickup', '$auto_dock_engage', '$teleop_upper_cone', '$teleop_middle_cone', '$teleop_lower_cone', '$teleop_upper_cube', '$teleop_middle_cube', '$teleop_lower_cube', '$teleop_pickup', '$links', '$early_endgame', '$endgame', '$num_docked', '$skill', '$defense', '$died', '$tipped', '$card', '$damage', '$comments', '$md5')";
     echo $insert;
 
-    try{$write = mysqli_query($conn, $insert);
+    try{$write = mysqli_query($conn, $insert);}
+    catch(Exception $e){
+	echo'<script>window.close();</script>';
     }
     //because some reason the duplicate protection fails, do it again
-    catch(Exception $e){
         //header('Location: index.html');
         //not needed since it auto-closes now
-    }
     if($write)
     {
         echo "<h1>Entries added!</h1><p>redirecting...</p>";
